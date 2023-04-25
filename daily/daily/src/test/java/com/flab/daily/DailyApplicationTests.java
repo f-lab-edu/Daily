e50b62ba3ccaa8c32ea96a10a8937bfd1f7f287d
l-lnaln <gusdk1793@gmail.com>
2023-04-25 23:11:46 +0900

    Daily Project 생성

---
+++ daily/daily/src/test/java/com/flab/daily/DailyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.daily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
