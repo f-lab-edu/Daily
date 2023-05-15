1ffbe5fdbf92683a1508a6fed880a0bef3ba5144
erinleeme <gusdk1793@gmail.com>
2023-05-15 12:42:10 +0900

    why..

--- src/main/java/com/flab/daily/MainbranchPush.java
+++ src/main/java/com/flab/daily/MainbranchPush.java
@@ -3,6 +3,6 @@ package com.flab.daily;
 public class MainbranchPush {
 
     public static void main(String[] args) {
-        System.out.println("why push..?!");
+        System.out.println("why....");
     }
 }
