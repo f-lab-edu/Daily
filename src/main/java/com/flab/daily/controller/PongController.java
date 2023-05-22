b0c1fa07704325dc34136a597bd48ba6a683b8af
erinleeme <gusdk1793@gmail.com>
2023-05-22 23:43:20 +0900

    [#23]feature-#23:Controller Unit 작성

--- src/main/java/com/flab/daily/controller/PongController.java
+++ src/main/java/com/flab/daily/controller/PongController.java
@@ -2,9 +2,7 @@ package com.flab.daily.controller;
 
 import com.flab.daily.service.PongService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
