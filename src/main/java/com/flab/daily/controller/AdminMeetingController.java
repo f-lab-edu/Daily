92227debe1de9f839775b5b5c6bc04a0922f27f1
erinleeme <gusdk1793@gmail.com>
2023-05-27 20:25:49 +0900

    [#25]feature-#25:mapper.xml, mapper, service 작성

--- src/main/java/com/flab/daily/controller/AdminMeetingController.java
+++ src/main/java/com/flab/daily/controller/AdminMeetingController.java
@@ -1,24 +1,27 @@
 package com.flab.daily.controller;
 
-import com.flab.daily.dto.request.MeetingRequestDTO;
+import com.flab.daily.dto.MeetingDTO;
 import com.flab.daily.service.AdminMeetingService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/admin")
-@RestController
 @RequiredArgsConstructor
+@RestController
 public class AdminMeetingController {
 
     private final AdminMeetingService adminMeetingService;
 
+
     @PostMapping("/meetings")
-    public ResponseEntity<Object> addMeeting(@Valid @RequestBody MeetingRequestDTO meetingRequestDTO) {
-        adminMeetingService.addMeeting(meetingRequestDTO);
-        return ResponseEntity.status(HttpStatus.CREATED).build(); // 201 Created.
+    public void addMeeting(@RequestBody MeetingDTO meetingDTO) {
+
+
+
     }
+
 
 }
--- src/main/java/com/flab/daily/service/AdminMeetingService.java
+++ src/main/java/com/flab/daily/service/AdminMeetingService.java
@@ -1,33 +1,39 @@
 package com.flab.daily.service;
 
-import com.flab.daily.dao.MeetingDAO;
-import com.flab.daily.dto.request.MeetingRequestDTO;
+
+import com.flab.daily.dto.MeetingDTO;
 import com.flab.daily.mapper.MeetingMapper;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
 public class AdminMeetingService {
 
     private final MeetingMapper meetingMapper;
+    private final HttpSession httpSession;
 
-    public void addMeeting(MeetingRequestDTO meetingRequestDTO) {
+    @Transactional
+    public void addMeeting(MeetingDTO meetingDTO) {
 
-        MeetingDAO meetingInfo = MeetingDAO.builder()
-                .categoryId(meetingRequestDTO.getCategoryId())
-                .meetingName(meetingRequestDTO.getMeetingName())
-                .meetingDescription(meetingRequestDTO.getMeetingDescription())
-                .meetingDate(meetingRequestDTO.getMeetingDate())
-                .meetingPlace(meetingRequestDTO.getMeetingPlace())
-                .meetingPeople(meetingRequestDTO.getMeetingPeople())
-                .meetingImage(meetingRequestDTO.getMeetingImage())
-                .createdBy(meetingRequestDTO.getCreatedBy())
+        // HttpSession 통해서 created_by 계정 추가
+        String email = httpSession.getAttribute("email") + "";
+
+        MeetingDTO meetingInfo = MeetingDTO.builder()
+                        .category_id(meetingDTO.getCategory_id())
+                        .meeting_name(meetingDTO.getMeeting_name())
+                        .meeting_description(meetingDTO.getMeeting_description())
+                        .meeting_date(meetingDTO.getMeeting_date()) //date 계산 필요
+                        .meeting_place(meetingDTO.getMeeting_place())
+                        .meeting_people(meetingDTO.getMeeting_people())
+                        .meeting_image(meetingDTO.getMeeting_image())
+                        .created_by(email)
                 .build();
 
-
         meetingMapper.addMeeting(meetingInfo);
+
     }
+
 }
