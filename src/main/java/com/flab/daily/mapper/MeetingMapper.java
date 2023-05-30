ce14339c67a5cf493eff352f0761b1be502b3f34
erinleeme <gusdk1793@gmail.com>
2023-05-30 21:35:55 +0900

    Mapper 클래스 못찾는 에러 해결

--- src/main/java/com/flab/daily/mapper/MeetingMapper.java
+++ src/main/java/com/flab/daily/mapper/MeetingMapper.java
@@ -6,12 +6,11 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Map;
 
-@Repository
 @Mapper
+@Repository
 public interface MeetingMapper {
 
     // void addMeeting(Map<String, String> map);
-
 
     MeetingDTO getMeeting();
 }
