033db7ac048b398712ccc2a14ac75116bd2c8bdf
erinleeme <gusdk1793@gmail.com>
2023-05-31 00:54:18 +0900

    [#25]feature-#25:@valid 적용 결과 확인

--- src/main/java/com/flab/daily/controller/AdminMeetingController.java
+++ src/main/java/com/flab/daily/controller/AdminMeetingController.java
@@ -2,7 +2,9 @@ package com.flab.daily.controller;
 
 import com.flab.daily.dto.MeetingDTO;
 import com.flab.daily.service.AdminMeetingService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/admin")
@@ -13,10 +15,17 @@ public class AdminMeetingController {
     private final AdminMeetingService adminMeetingService;
 
     @PostMapping("/meetings")
-    public void addMeeting(@RequestBody MeetingDTO meetingDTO) {
-        adminMeetingService.addMeeting(meetingDTO);
+    public void addMeeting(@RequestBody @Valid MeetingDTO meetingDTO, Errors errors) {
+
+        if (!errors.hasErrors()) {
+            adminMeetingService.addMeeting(meetingDTO);
+        } else {
+            System.out.println(errors.getFieldError().getDefaultMessage()); // error message test
+        }
     }
 
+
+    //MariaDB 연결 테스트용 - meeting 테이블에 있는 데이터 반환 메서드
     @GetMapping("/meetings")
     public MeetingDTO getMeeting() {
         return adminMeetingService.getMeeting();
