f64e0ecce5a5ff1d037e944088986496ea1c908a
erinleeme <gusdk1793@gmail.com>
2023-06-15 20:23:49 +0900

    [#25]feature-#25:AdminMeetingServiceTest 추가

--- src/test/java/com/flab/daily/service/AdminMeetingServiceTest.java
+++ src/test/java/com/flab/daily/service/AdminMeetingServiceTest.java
@@ -3,6 +3,7 @@ package com.flab.daily.service;
 import com.flab.daily.dto.request.MeetingRequestDTO;
 import com.flab.daily.exception.IsExistCheckException;
 import com.flab.daily.mapper.CategoryMapper;
+import com.flab.daily.mapper.MemberMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,42 +23,57 @@ public class AdminMeetingServiceTest {
     @Mock
     CategoryMapper categoryMapper;
 
+    @Mock
+    MemberMapper memberMapper;
+
     @InjectMocks
     AdminMeetingService adminMeetingService;
     MeetingRequestDTO meetingRequestDTO;
 
     @BeforeEach
     void beforeEach() {
+        //Test에 사용될 DTO
         meetingRequestDTO = MeetingRequestDTO.builder()
-                .categoryId(2) // Invalid categoryId 확인을 위해 잘못된 값 주입
+                .categoryId(1)
                 .meetingName("축구하기")
                 .meetingDescription("축구 같이 하실래요?")
                 .meetingDate(LocalDateTime.now().plusDays(20))
                 .meetingPlace("잠실운동장")
                 .meetingPeople(10)
                 .meetingImage(null)
-                .createdBy("1234@gmail.com")
+                .createdBy("test@naver.com")
                 .build();
     }
 
-
-
-    //해당 카테고리 Id가 없으면 IsExistCheckException 발생
+    //DB에 저장된 유효한 카테고리 아이디값이 맞는지 체크하는 함수
     @Test
     @DisplayName("Invalid CategoryId Check.")
-    void test_Invalid_CategoryId__False() {
-
+    void test_Invalid_CategoryId_False() {
         //given
-        when(categoryMapper.isValidExist(meetingRequestDTO.getCategoryId())).thenReturn(0); //카테고리값이 유효하지 않으면 0반환
-
+        //categoryMapper.isValidExist를 호출하였을 때 에러 발생하도록 설정
+        when(categoryMapper.isValidExist(meetingRequestDTO.getCategoryId())).thenReturn(0);
+        //memberMapper.isValidExist를 호출하였을 때 정상 설정
+        when(memberMapper.isValidExist(meetingRequestDTO.getCreatedBy())).thenReturn(1);
         //when-then
         assertThrows(IsExistCheckException.class, () -> adminMeetingService.addMeeting(meetingRequestDTO));
     }
 
-
-    //invalid email check
-
+    //DB에 저장된 Email이 체크하는 함수
+    @Test
+    @DisplayName("Invalid Email Check.")
+    void test_Invalid_Email_False() {
+        //given
+        //categoryMapper.isValidExist를 호출하였을 때 정상 설정
+        when(categoryMapper.isValidExist(meetingRequestDTO.getCategoryId())).thenReturn(1);
+        //memberMapper.isValidExist를 호출하였을 때 에러 발생하도록 설정
+        when(memberMapper.isValidExist(meetingRequestDTO.getCreatedBy())).thenReturn(0);
+        //when-then
+        assertThrows(IsExistCheckException.class, () -> adminMeetingService.addMeeting(meetingRequestDTO));
+    }
+    
     //DB 서버 다운되었을 때 처리 check
+
+    
 
     //SQL Exception
 
