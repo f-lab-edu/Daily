51d9e5139fad36c49b2c807bb80bba8f6dbda319
rhichu89 <rhichu89@gmail.com>
2023-06-17 20:24:26 +0900

    [#24]feature-#24: RequestDTO 수정 및 connection 에러 추가

--- src/main/java/com/flab/daily/dto/request/MemberRequestDTO.java
+++ src/main/java/com/flab/daily/dto/request/MemberRequestDTO.java
@@ -11,13 +11,26 @@ import lombok.Data;
 @Builder
 public class MemberRequestDTO {
     // TODO: 패턴 두개 비교 필요
-//    @Pattern(regexp = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\\\.[A-Za-z0-9_-]+)*@[^-][A-Za- z0-9-]+(\\\\.[A-Za-z0-9-]+)*(\\\\.[A-Za-z]{2,})$")
-    @Email(regexp = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}",
+
+    /**
+     *  CASE_INSENSITIVE : 대소문자를 구분하지 않는 일치를 활성화
+     *  https://docs.oracle.com/javase/6/docs/api/java/util/regex/Pattern.html#CASE_INSENSITIVE
+     *  [a-z0-9]+ : 영어 소문자, 1~9까지 숫자만 허용하는 규칙이 최소 한개 이상
+     *  \\.[a-z]{2,3} : .이후 영어 소문자 2~3 자리
+     */
+    @Email(regexp = "[a-z0-9]+@[a-z0-9.]+\\.[a-z]{2,3}",
             flags = Pattern.Flag.CASE_INSENSITIVE,
             message = "이메일 형식에 맞지 않습니다")
     private String email;
 
 
+    /**
+     * ^ : 뒤에나오는 패턴이 일치한 경우
+     * (?=.*\d) : 숫자를 사용한 패턴이 하위에 있는지 확인
+     * (?=.*[a-z|A-Z]) : 영문을 사용한 패턴이 있는지 확인
+     * (?=.*[^\w\d\s:]) : 문자, 숫자, 공백, : 조합이 아닌경우
+     *  ([^\s]){8,16}$ : 공백 없이 8~ 16 글자로 끝나는
+     */
     @Pattern(regexp = "^(?=.*\\d)(?=.*[a-z|A-Z])(?=.*[^\\w\\d\\s:])([^\\s]){8,16}$"
             , message = "영문, 숫자, 특수문자 중 3종류 이상을 조합하여 최소 8자 이상 입력해 주세요")
     private String password;
--- src/main/java/com/flab/daily/exception/ErrorHandler.java
+++ src/main/java/com/flab/daily/exception/ErrorHandler.java
@@ -1,9 +1,7 @@
 package com.flab.daily.exception;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.net.ConnectException;
