21f3f0942495cd48886304affac65360124ad5d7
erinleeme <gusdk1793@gmail.com>
2023-06-23 15:22:30 +0900

    [#35]feature-#35:Request Parameter Exception Handler

--- src/main/java/com/flab/daily/exception/ErrorCode.java
+++ src/main/java/com/flab/daily/exception/ErrorCode.java
@@ -7,6 +7,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
 
+    INVALID_INPUT_PARAMETER(700, "잘못된 파라미터 값입니다."),
+    INVALID_DATATYPE(701, "잘못된 데이터 타입입니다."),
+
     NOT_FOUND_CATEGORY(900,"유효하지 않은 카테고리입니다."),
     NOT_FOUND_EMAIL(900,"유효하지 않는 Email입니다."),
     NOT_FOUND_MEETING(900,"유효하지 않는 소모임입니다."),
--- src/main/java/com/flab/daily/exception/ErrorHandler.java
+++ src/main/java/com/flab/daily/exception/ErrorHandler.java
@@ -7,12 +7,13 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @Slf4j
 @RestControllerAdvice
 public class ErrorHandler {
 
-    //@Valid Exception 확인 메소드
+    //@Valid Exception Handler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ErrorResponse invalidRequestHandler(MethodArgumentNotValidException e) {
@@ -20,7 +21,7 @@ public class ErrorHandler {
         return response;
     }
 
-    //데이터가 DB에 없을 때 발생하는 메소드
+    //데이터가 DB에 없을 때 Exception Handler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(IsExistCheckException.class)
     public ErrorResponse isExistCheckExceptionHandler(IsExistCheckException e) {
@@ -28,7 +29,7 @@ public class ErrorHandler {
         return response;
     }
 
-    //데이터 중복시 발생하는 메소드
+    //데이터 중복 Exception Handler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(DuplicateCheckException.class)
     public ErrorResponse duplicateCheckExceptionHandler(DuplicateCheckException e) {
@@ -36,11 +37,21 @@ public class ErrorHandler {
         return response;
     }
 
-    //타입 일치 Exception 메소드
+    //타입 불일치 Exception
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ErrorResponse notReadableExceptionHandler(HttpMessageNotReadableException e) {
-        ErrorResponse response = new ErrorResponse(400,  "데이터 타입이 맞지 않습니다.");
+        ErrorResponse response = ErrorResponse.builder().code(ErrorCode.INVALID_DATATYPE.getCode())
+                .message(ErrorCode.NOT_FOUND_MEETING.getMessage()).build();
+        return response;
+    }
+
+    //잘못된 파라미터값 Exception
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ErrorResponse notReadableExceptionHandler(MethodArgumentTypeMismatchException e) {
+        ErrorResponse response = ErrorResponse.builder().code(ErrorCode.INVALID_INPUT_PARAMETER.getCode())
+                .message(ErrorCode.INVALID_INPUT_PARAMETER.getMessage()).build();
         return response;
     }
 }
