9176e16ce1d2818a1eaae9f97168f8dea3d74f63
l-lnaln <gusdk1793@gmail.com>
2023-06-24 16:47:06 +0900

    [#36]feature-#36: 소모임 목록 조회 API main 작성

--- src/main/java/com/flab/daily/mapper/MeetingMapper.java
+++ src/main/java/com/flab/daily/mapper/MeetingMapper.java
@@ -4,9 +4,13 @@ import com.flab.daily.dao.MeetingDAO;
 import com.flab.daily.dto.response.MeetingResponseDTO;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface MeetingMapper {
     int addMeeting(MeetingDAO meetingDAO);
+
+    List<MeetingResponseDTO> findMeetingList();
 
     MeetingResponseDTO findMeetingOneById(Long meetingId);
 }
