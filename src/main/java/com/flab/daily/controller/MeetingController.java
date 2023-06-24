8f645bc0f046d1a486014d2c921b17b1ad628cfa
l-lnaln <gusdk1793@gmail.com>
2023-06-24 16:47:06 +0900

    [#36]feature-#36: 소모임 목록 조회 API main 작성

--- src/main/java/com/flab/daily/controller/MeetingController.java
+++ src/main/java/com/flab/daily/controller/MeetingController.java
@@ -8,17 +8,29 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/meeting")
 public class MeetingController {
 
     private final MeetingService meetingService;
 
-    @GetMapping("/meeting/{meetingId}")
+    @GetMapping
+    public ResponseEntity<List<MeetingResponseDTO>> findMeetingList() {
+        List<MeetingResponseDTO> meetingList = meetingService.findMeetingList();
+        return ResponseEntity.status(HttpStatus.OK).body(meetingList);
+    }
+
+    @GetMapping("/{meetingId}")
     public ResponseEntity<MeetingResponseDTO> findMeetingOneById(@PathVariable @Valid Long meetingId) {
         MeetingResponseDTO meetingResponseDTO = meetingService.findMeetingOneById(meetingId);
         return ResponseEntity.status(HttpStatus.OK).body(meetingResponseDTO);
     }
+
+
 }
