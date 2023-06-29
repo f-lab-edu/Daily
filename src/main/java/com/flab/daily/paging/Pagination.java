6f6fefbbf0b110baa3b9d03f6c1d5f56cabbe2d0
erinleeme <gusdk1793@gmail.com>
2023-06-29 13:51:14 +0900

    [#36]feature-#36:소모임 목록 조회 Paging 처리 추가

--- src/main/java/com/flab/daily/paging/Pagination.java
+++ src/main/java/com/flab/daily/paging/Pagination.java
@@ -1,34 +1,34 @@
 package com.flab.daily.paging;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 
 @Getter
 public class Pagination {
+    private int size; /*한 페이지당 보여질 데이터 갯수*/
+    private int page; /*접속한 페이지*/
+    private int startSize; /*시작할 데이터 순서*/
 
-    private int totalSize;
-    private int totalPage;
-    private int startPage;
-    private int endPage;
-    private int limit;
-    private boolean prevPage;
-    private boolean nextPage;
+    private int totalSize = 0; /*데이터 전체 수*/
+    private int totalPage = 0; /*전체 페이지 수*/
+    private int currentPage = 0; /*현재 페이지*/
+    private boolean prevPage = false; /*이전 페이지 유무*/
+    private boolean nextPage= false; /*다음 페이지 유무*/
 
-    public Pagination(int totalSize, Pageable pageable) {
-        if(totalSize > 0) {
-            this.totalSize = totalSize;
-            calculate(pageable);
-        }
+    public Pagination (int totalSize, int size, int page) {
+        this.size = size;
+        this.page = page;
+        this.totalSize = totalSize;
+        this.startSize = size * (page-1);
+        this.currentPage = page;
+        calculate();
     }
 
-    private void calculate(Pageable pageable) {
-        totalPage = ((totalSize-1)/pageable.getSize()+1);
-        if(pageable.getPage() > totalPage) {
-            pageable.setPage(totalPage);
-        }
-
-        limit = (pageable.getPage()-1)*pageable.getSize();
-
-        prevPage = (startPage != 1);
-        nextPage = (endPage*pageable.getSize()) < totalSize;
+    private void calculate() {
+        totalPage = ((totalSize-1)/size+1); /*전체 페이지 추출*/
+        prevPage = (currentPage != 1); /*현재 페이지가 1이 아니면 true*/
+        nextPage = (currentPage < totalPage); /*현재 페이지가 전체 페이지 수보다 작으면 true*/
     }
 }
