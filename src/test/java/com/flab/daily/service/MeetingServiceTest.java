bf3a221bca720b97a94c2b498ecc02614ab4296c
erinleeme <gusdk1793@gmail.com>
2023-07-04 19:39:44 +0900

    [#36]feature-#36:Test 코드 작성

--- src/test/java/com/flab/daily/service/MeetingServiceTest.java
+++ src/test/java/com/flab/daily/service/MeetingServiceTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
