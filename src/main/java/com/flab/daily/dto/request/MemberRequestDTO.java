bf1765f8a4af7535d048b974821083e6d3870bfe
kwj0318 <rhichu89@gmail.com>
2023-07-04 23:22:38 +0900

    Revert "[#24]feature-#24:  멤버 삽입시 결함 수정 및 업데이트 시간 삽입, DTO JsonProperty 적용"
    This reverts commit 59076f44e258d2270d1d629aa23166836c884d52.

--- src/main/java/com/flab/daily/dto/request/MemberRequestDTO.java
+++ src/main/java/com/flab/daily/dto/request/MemberRequestDTO.java
@@ -1,6 +1,5 @@
 package com.flab.daily.dto.request;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.flab.daily.type.MemberType;
 import com.flab.daily.validator.EnumValidator;
 import jakarta.validation.constraints.Email;
@@ -43,7 +42,6 @@ public class MemberRequestDTO {
     @NotBlank(message = "닉네임은 필수 값입니다.")
     private String nickname;
 
-    @JsonProperty(value = "member_type")
     @NotNull(message = "맴버 타입은 필수 값입니다.")
     @EnumValidator(enumClass = MemberType.class, message = "멤버 형식이 일치하지 않습니다.")
     private MemberType memberType;
--- src/main/java/com/flab/daily/service/MemberService.java
+++ src/main/java/com/flab/daily/service/MemberService.java
@@ -2,9 +2,10 @@ package com.flab.daily.service;
 
 import com.flab.daily.dao.MemberDAO;
 import com.flab.daily.dto.request.MemberRequestDTO;
-import com.flab.daily.exception.ErrorCode;
 import com.flab.daily.exception.IsExistUserByEmail;
+import com.flab.daily.exception.ErrorCode;
 import com.flab.daily.mapper.MemberMapper;
+import com.flab.daily.type.MemberType;
 import com.flab.daily.utils.SHA256Util;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,12 +21,14 @@ public class MemberService {
 
     public void signUp(MemberRequestDTO memberRequestDTO) {
         int isMember = memberMapper.getMember(memberRequestDTO.getEmail());
-        if (isMember > 0) throw new IsExistUserByEmail(ErrorCode.IS_EXIST_USER_BY_EMAIL);
+        if (isMember > 0) {
+            throw new IsExistUserByEmail(ErrorCode.IS_EXIST_USER_BY_EMAIL);
+        }
         MemberDAO memberDAO = MemberDAO.builder()
                 .email(memberRequestDTO.getEmail())
                 .password(SHA256Util.encrypt(memberRequestDTO.getPassword()))
                 .nickname(memberRequestDTO.getNickname())
-                .memberType(memberRequestDTO.getMemberType())
+                .memberType(MemberType.USER)
                 .build();
         memberMapper.insertMember(memberDAO);
     }
