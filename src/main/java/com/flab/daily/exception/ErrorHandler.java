3b92231fb3ea331885f40ada3c0c548677572ef7
erinleeme <gusdk1793@gmail.com>
2023-07-06 08:51:53 +0900

    [#36]feature-#36:회원등록 API와 merge

--- src/main/java/com/flab/daily/exception/ErrorHandler.java
+++ src/main/java/com/flab/daily/exception/ErrorHandler.java
@@ -2,6 +2,7 @@ package com.flab.daily.exception;
 
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
