7ec52d3e242416303edc05fb30e65cccfd8abb9a
kwj0318 <rhichu89@gmail.com>
2023-07-26 02:58:19 +0900

    [#49]feature-#49: 카테고리 조회 API

--- src/main/java/com/flab/daily/mapper/CategoryMapper.java
+++ src/main/java/com/flab/daily/mapper/CategoryMapper.java
@@ -1,10 +1,15 @@
 package com.flab.daily.mapper;
 
 import com.flab.daily.dao.CategoryDAO;
+import com.flab.daily.dao.Pagination;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface CategoryMapper {
+    Long getCategoryListTotal();
+    List<CategoryDAO> getCategoryList(Pagination pagination);
     int isExistCategoryById(Long categoryId);
     int isExistCategoryByName(String categoryName);
     int addCategory(CategoryDAO categoryDAO);
