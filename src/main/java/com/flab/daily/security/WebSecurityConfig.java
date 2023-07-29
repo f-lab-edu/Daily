aec8414662bc51ee4e5bf40df75222e3cb8a05b1
l-lnaln <gusdk1793@gmail.com>
2023-07-29 16:41:43 +0900

    [#50]feature-#50: 스프링 시큐리티 로그인 구현

--- src/main/java/com/flab/daily/security/WebSecurityConfig.java
+++ src/main/java/com/flab/daily/security/WebSecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
