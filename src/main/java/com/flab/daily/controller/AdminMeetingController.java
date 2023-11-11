8e8dc34281447c6fdd45df3186ef570b8599f14b
TOG B <4ir@BTOG-MacBook-Pro.local>
2023-11-11 18:48:12 +0900

    [#66]feature-#66:소모임 정보 수정 API 생성 시작

--- src/main/java/com/flab/daily/controller/AdminMeetingController.java
+++ src/main/java/com/flab/daily/controller/AdminMeetingController.java
@@ -1,7 +1,11 @@
 package com.flab.daily.controller;
 
 import com.flab.daily.dto.request.MeetingRequestDTO;
+import com.flab.daily.dto.response.MeetingResponseDTO;
 import com.flab.daily.service.AdminMeetingService;
+import com.flab.daily.utils.dto.ResponseDTO;
+import com.flab.daily.utils.security.jwt.JwtProvider;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,11 +18,21 @@ import org.springframework.web.bind.annotation.*;
 public class AdminMeetingController {
 
     private final AdminMeetingService adminMeetingService;
+    private final JwtProvider jwtProvider;
 
     @PostMapping("/meetings")
     public ResponseEntity<Object> addMeeting(@Valid @RequestBody MeetingRequestDTO meetingRequestDTO) {
         adminMeetingService.addMeeting(meetingRequestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PatchMapping("/meetings/{meetingId}")
+    public ResponseEntity<MeetingResponseDTO> updateMeetingInfo(@Valid @RequestBody MeetingRequestDTO meetingRequestDTO,
+            , @PathVariable Long meetingId, HttpServletRequest request) {
+        String updatedEmail = jwtProvider.getEmailByAccessToken(request);
+        MeetingResponseDTO meetingResponseDTO = adminMeetingService.updateMeetingInfo(meetingId, updatedEmail, meetingRequestDTO);
+        return ResponseEntity.status(HttpStatus.OK).body(meetingResponseDTO);
+
     }
 
 }
